38da6b4ce31dac1e7e34cdaa7703a24a7e4522d6
Henrique Doro <opensource@hdoro.dev>
2022-09-28 18:26:24 -0300

    feat: install tailwind

--- src/DocumentNode.tsx
+++ src/DocumentNode.tsx
@@ -1,9 +1,10 @@
 import { Handle, Node, Position } from 'react-flow-renderer'
 import { FileInfo, useStore } from './store'
 import Tiptap from './Tiptap'
+import icons from './icons'
 
 export function DocumentNode({ data, selected, ...props }: Node<FileInfo>) {
-  const { editFile } = useStore()
+  const { editFile, removeFilesFromCanvas } = useStore()
 
   return (
     <>
@@ -17,13 +18,27 @@ export function DocumentNode({ data, selected, ...props }: Node<FileInfo>) {
           width: '20vw',
         }}
       >
+        {/* Header */}
+        <div className="border-b-2 border-gray-200">
+          <button
+            onClick={() => removeFilesFromCanvas([data.fileId])}
+            className="text-gray-700 hover:text-red-800"
+          >
+            <span className="sr-only">Delete</span>
+            <icons.Delete className="w-[1em] " />
+          </button>
+        </div>
+
+        {/* Hacky way of showing arrows: opacity = 0 on handles. W/o handles react-flow doesn't render edges */}
         <Handle
           type="source"
           position={Position.Right}
           style={{ opacity: 0 }}
+          hidden={true}
         />
         <Handle type="target" position={Position.Left} style={{ opacity: 0 }} />
-        <pre>{data.fileName}</pre>
+
+        <pre className="text-lg">{data.fileName}</pre>
         <Tiptap
           content={data.content}
           onChange={(content) =>
