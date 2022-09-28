b811fc85ddb79db60ab9860a2dc21d20d9fadc06
Henrique Doro <opensource@hdoro.dev>
2022-09-28 17:39:36 -0300

    feat: starting to render TipTap

--- src/DocumentNode.tsx
+++ src/DocumentNode.tsx
@@ -1,23 +1,20 @@
-import { Box, Modal, Typography } from "@mui/material";
-import Button from "@mui/material/Button";
-import { marked } from "marked";
-import { useState } from "react";
-import { Handle, Node, Position } from "react-flow-renderer";
-import { FileInfo, useStore } from "./store";
+import { Handle, Node, Position } from 'react-flow-renderer'
+import { FileInfo, useStore } from './store'
+import Tiptap from './Tiptap'
 
 export function DocumentNode({ data, selected, ...props }: Node<FileInfo>) {
-  const [status, setStatus] = useState("idle");
-  const { removeFilesFromCanvas } = useStore();
+  const { editFile } = useStore()
 
   return (
     <>
       <div
         style={{
-          background: "white",
-          borderRadius: ".35em",
-          border: selected ? "2px solid #333" : "2px solid #888",
-          padding: "1em",
-          fontSize: ".7rem",
+          background: 'white',
+          borderRadius: '.35em',
+          border: selected ? '2px solid #333' : '2px solid #888',
+          padding: '1em',
+          fontSize: '.7rem',
+          width: '20vw',
         }}
       >
         <Handle
@@ -27,61 +24,16 @@ export function DocumentNode({ data, selected, ...props }: Node<FileInfo>) {
         />
         <Handle type="target" position={Position.Left} style={{ opacity: 0 }} />
         <pre>{data.fileName}</pre>
-        {data.contents && (
-          <div
-            dangerouslySetInnerHTML={{ __html: marked.parse(data.contents) }}
-          ></div>
-        )}
-        {selected && (
-          <div className="card--actions">
-            <Button
-              variant="outlined"
-              onClick={() => {
-                removeFilesFromCanvas([data.fileId]);
-              }}
-              color="error"
-            >
-              Remove
-            </Button>
-            <Button
-              variant="outlined"
-              onClick={() => {
-                setStatus("editing");
-              }}
-              color="primary"
-            >
-              Edit
-            </Button>
-          </div>
-        )}
-        <Modal
-          open={status === "editing"}
-          onClose={() => setStatus("idle")}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box
-            sx={{
-              position: "absolute" as "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              width: 400,
-              bgcolor: "background.paper",
-              border: "2px solid #000",
-              boxShadow: 24,
-              p: 4,
-            }}
-          >
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Edição aqui!
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              (explicação do arquivo)
-            </Typography>
-          </Box>
-        </Modal>
+        <Tiptap
+          content={data.content}
+          onChange={(content) =>
+            editFile({
+              fileId: data.fileId,
+              content,
+            })
+          }
+        />
       </div>
     </>
-  );
+  )
 }
--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,4 @@
+import { JSONContent } from '@tiptap/react'
 import {
   addEdge,
   applyEdgeChanges,
@@ -12,147 +13,171 @@ import {
   OnNodesChange,
   ReactFlowInstance,
   XYPosition,
-} from "react-flow-renderer";
-import create from "zustand";
+} from 'react-flow-renderer'
+import create from 'zustand'
+import { persist } from 'zustand/middleware'
 
 export interface FileInfo {
-  fileId: string;
-  fileName: string;
-  // Markdown?
-  contents: string;
-  references?: string[];
+  fileId: string
+  fileName: string
+  content?: JSONContent
+  references?: string[]
 }
 
 type RFState = {
-  nodes: Node<FileInfo>[];
-  unaddedFiles: FileInfo[];
-  edges: Edge[];
-  onNodesChange: OnNodesChange;
-  onEdgesChange: OnEdgesChange;
-  onConnect: OnConnect;
+  nodes: Node<FileInfo>[]
+  unaddedFiles: FileInfo[]
+  edges: Edge[]
+  onNodesChange: OnNodesChange
+  onEdgesChange: OnEdgesChange
+  onConnect: OnConnect
+  editFile: (
+    updatedFile: Partial<Omit<FileInfo, 'fileId'>> & Pick<FileInfo, 'fileId'>,
+  ) => void
   addFilesToCanvas: (
     nodes: {
-      fileId: FileInfo["fileId"];
-      position: XYPosition;
-    }[]
-  ) => void;
-  removeFilesFromCanvas: (nodeIds: FileInfo["fileId"][]) => void;
-  reactFlowInstance: ReactFlowInstance | null;
-  setReactFlowInstance: (instance: ReactFlowInstance) => void;
-};
+      fileId: FileInfo['fileId']
+      position: XYPosition
+    }[],
+  ) => void
+  removeFilesFromCanvas: (nodeIds: FileInfo['fileId'][]) => void
+  reactFlowInstance: ReactFlowInstance | null
+  setReactFlowInstance: (instance: ReactFlowInstance) => void
+}
 
 // this is our useStore hook that we can use in our components to get parts of the store and call actions
-export const useStore = create<RFState>((set, get) => ({
-  nodes: [
-    {
-      id: "asjd912390",
-      type: "document",
-      data: {
-        fileId: "asjd912390",
-        fileName: "index.tsx",
-        contents: "# App's entry point\nThis file is concerned (...)",
-      },
-      position: { x: 250, y: 5 },
-    },
-  ],
-  unaddedFiles: [
-    {
-      fileId: "asjasdd912390",
-      fileName: "App.tsx",
-      contents: "# React app",
-    },
-    {
-      fileId: "aasdksjasdd9123390",
-      fileName: "queries.ts",
-      contents: "# Sidebar for dragging files",
-    },
-    {
-      fileId: "aasdksjasdd912390",
-      fileName: "Sidebar.tsx",
-      contents: "# Sidebar for dragging files",
-    },
-    {
-      fileId: "aasdasdg1ksjasdd912390",
-      fileName: "package.json",
-      contents: "# Sidebar for dragging files",
-    },
-  ],
-  edges: [],
-  addFilesToCanvas: (newFiles = []) => {
-    const curState = get();
-    const newFileIds = newFiles.map((f) => f.fileId);
-    const newNodes = newFiles
-      .map((newFile) => {
-        const fileData = curState.unaddedFiles.find(
-          (unaddedFile) => unaddedFile.fileId === newFile?.fileId
-        );
-
-        if (!newFile || !curState.reactFlowInstance || !fileData?.fileId) {
-          return;
-        }
-
-        return {
-          id: fileData?.fileId,
-          type: "document",
-          position: curState.reactFlowInstance.project(newFile.position),
-          data: fileData,
-        };
-      })
-      .filter((node) => !!node?.id) as RFState["nodes"];
-
-    const edges = [...curState.nodes, ...newNodes].reduce(
-      (edges, curNode, index, arr) => {
-        return [
-          ...edges,
-          {
-            source: curNode.id,
-            target: arr[index + 1]?.id,
-            // Create the id from (...)
-            id: `${curNode.id}--${arr[index + 1]?.id}`,
+export const useStore = create(
+  persist<RFState>(
+    (set, get) => ({
+      nodes: [
+        {
+          id: 'asjd912390',
+          type: 'document',
+          data: {
+            fileId: 'asjd912390',
+            fileName: 'index.tsx',
           },
-        ].filter((edge) => !!edge.target);
-      },
-      [] as Edge[]
-    );
-
-    set({
-      nodes: [...curState.nodes, ...newNodes],
-      edges,
-      // @TODO: auto-parse unadded & added files based on database query
-      unaddedFiles: curState.unaddedFiles.filter(
-        (file) => !newFileIds.includes(file.fileId)
-      ),
-    });
-  },
-  removeFilesFromCanvas: (nodeIds) => {
-    const inCanvas = get().nodes.filter((node) => !nodeIds.includes(node.id));
-    const offCanvas = get().nodes.filter((node) => nodeIds.includes(node.id));
-    set({
-      nodes: inCanvas,
-      unaddedFiles: [
-        ...get().unaddedFiles,
-        ...offCanvas.map((item) => item.data),
+          position: { x: 250, y: 5 },
+        },
       ],
-    });
-  },
-  onNodesChange: (changes: NodeChange[]) => {
-    set({
-      nodes: applyNodeChanges(changes, get().nodes),
-    });
-  },
-  onEdgesChange: (changes: EdgeChange[]) => {
-    set({
-      edges: applyEdgeChanges(changes, get().edges),
-    });
-  },
-  onConnect: (connection: Connection) => {
-    set({
-      edges: addEdge(connection, get().edges),
-    });
-  },
-  reactFlowInstance: null,
-  setReactFlowInstance: (instance) =>
-    set({
-      reactFlowInstance: instance,
+      unaddedFiles: [
+        {
+          fileId: 'asjasdd912390',
+          fileName: 'App.tsx',
+        },
+        {
+          fileId: 'aasdksjasdd9123390',
+          fileName: 'queries.ts',
+        },
+        {
+          fileId: 'aasdksjasdd912390',
+          fileName: 'Sidebar.tsx',
+        },
+        {
+          fileId: 'aasdasdg1ksjasdd912390',
+          fileName: 'package.json',
+        },
+      ],
+      edges: [],
+      addFilesToCanvas: (newFiles = []) => {
+        const curState = get()
+        const newFileIds = newFiles.map((f) => f.fileId)
+        const newNodes = newFiles
+          .map((newFile) => {
+            const fileData = curState.unaddedFiles.find(
+              (unaddedFile) => unaddedFile.fileId === newFile?.fileId,
+            )
+
+            if (!newFile || !curState.reactFlowInstance || !fileData?.fileId) {
+              return
+            }
+
+            return {
+              id: fileData?.fileId,
+              type: 'document',
+              position: curState.reactFlowInstance.project(newFile.position),
+              data: fileData,
+            }
+          })
+          .filter((node) => !!node?.id) as RFState['nodes']
+
+        const edges = [...curState.nodes, ...newNodes].reduce(
+          (edges, curNode, index, arr) => {
+            return [
+              ...edges,
+              {
+                source: curNode.id,
+                target: arr[index + 1]?.id,
+                // Create the id from (...)
+                id: `${curNode.id}--${arr[index + 1]?.id}`,
+              },
+            ].filter((edge) => !!edge.target)
+          },
+          [] as Edge[],
+        )
+
+        set({
+          nodes: [...curState.nodes, ...newNodes],
+          edges,
+          // @TODO: auto-parse unadded & added files based on database query
+          unaddedFiles: curState.unaddedFiles.filter(
+            (file) => !newFileIds.includes(file.fileId),
+          ),
+        })
+      },
+      editFile: (updatedFile) => {
+        set({
+          nodes: get().nodes.map((node) => {
+            if (node.data.fileId !== updatedFile.fileId) return node
+
+            return {
+              ...node,
+              data: {
+                ...node.data,
+                ...updatedFile,
+              },
+            }
+          }),
+        })
+      },
+      removeFilesFromCanvas: (nodeIds) => {
+        const inCanvas = get().nodes.filter(
+          (node) => !nodeIds.includes(node.id),
+        )
+        const offCanvas = get().nodes.filter((node) =>
+          nodeIds.includes(node.id),
+        )
+        set({
+          nodes: inCanvas,
+          unaddedFiles: [
+            ...get().unaddedFiles,
+            ...offCanvas.map((item) => item.data),
+          ],
+        })
+      },
+      onNodesChange: (changes: NodeChange[]) => {
+        set({
+          nodes: applyNodeChanges(changes, get().nodes),
+        })
+      },
+      onEdgesChange: (changes: EdgeChange[]) => {
+        set({
+          edges: applyEdgeChanges(changes, get().edges),
+        })
+      },
+      onConnect: (connection: Connection) => {
+        set({
+          edges: addEdge(connection, get().edges),
+        })
+      },
+      reactFlowInstance: null,
+      setReactFlowInstance: (instance) =>
+        set({
+          reactFlowInstance: instance,
+        }),
     }),
-}));
+    {
+      name: 'docs-storage',
+    },
+  ),
+)
